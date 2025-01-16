7c090c45fe2c225ef9049301fe47907eb7baf013
Kalmyk72 <39917407+Kalmyk72@users.noreply.github.com>
2025-01-16 15:58:43 +0500

    Diplom version 0.1

--- ImageEditor/obj/Debug/net8.0-windows/ImageEditor.AssemblyInfo.cs
+++ ImageEditor/obj/Debug/net8.0-windows/ImageEditor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ImageEditor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ccc007f76204ad0a24bf904d749ccbb25be3359")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d475173c7e8a6b13dc65620a6a4d08cf69a669af")]
 [assembly: System.Reflection.AssemblyProductAttribute("ImageEditor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ImageEditor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
